9b1a6873be6c8ab48a4b89679582be81edeabb91
pewegit <pewegit@gmx.de>
2018-01-18 23:20:38 +0100

    rework

--- iban/MainWindow.xaml.cs
+++ iban/MainWindow.xaml.cs
@@ -23,14 +23,14 @@ namespace iban
             bool digitsOnly = blz.Length > 0 && kto.Length > 0 && blz.All(char.IsDigit) && kto.All(char.IsDigit);
             if ( digitsOnly )
             {
-                string ktoAsString10 = string.Format("{0:0000000000}", Convert.ToDecimal(TEXTBOX_KTO.Text)); // auf 10 Zeichen fixiert (führende Nullen, falls Kontonummer kürzer als 10 Stellen)
+                string ktoAsString10 = string.Format("{0:0000000000}", Convert.ToDecimal(TEXTBOX_KTO.Text)); // limit to 10 chars (leading zeros, if account number shorter than 10 chars)
                 string bban = blz + ktoAsString10 + "131400";
                 decimal bbanAsDec = Convert.ToDecimal(bban);
                 decimal chksum = bbanAsDec % 97;
                 chksum = 98 - chksum;
-                string chksumAsString = string.Format("{0:00}", chksum); // auf 2 Zeichen fixiert (eine führende Null, falls Prüfsumme nur einstellig)
+                string chksumAsString = string.Format("{0:00}", chksum); // fixed to 2 chars (one leading zero, if checksum has only 1 char)
                 string iban = "DE" + chksumAsString + blz + ktoAsString10;
-                //TEXTBOX_IBAN.Text = iban; // Nur diese Zeile nötig bei textbox (aber halt ohne Farbe)
+                //TEXTBOX_IBAN.Text = iban; // Only this line required for textbox, but without color
 
                 // Create a FlowDocument
                 FlowDocument mcFlowDoc = new FlowDocument();
@@ -62,7 +62,7 @@ namespace iban
 
                 // Set contents
                 RICHTEXTBOX_IBAN.Document = mcFlowDoc;
-                //TEXTBOX_IBAN.Text = "Nur Zahlen und keine Leerzeichen"; // Nur diese Zeile nötig bei textbox (aber halt ohne Farbe)
+                //TEXTBOX_IBAN.Text = "Nur Zahlen und keine Leerzeichen"; // Only this line required for textbox (but then without color)
             }
         }
     }
