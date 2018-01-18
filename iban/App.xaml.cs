65a84a11d0f2136c5d5e66ae1005a3416228629d
pewegit <pewegit@gmx.de>
2018-01-18 22:32:02 +0100

    Icon removed

--- iban/App.xaml.cs
+++ iban/App.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows;
 namespace iban
 {
     /// <summary>
-    /// Interaction logic for App.xaml modified
+    /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
